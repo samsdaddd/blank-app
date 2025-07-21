7b0bdb7192f9678a2fd868e9578199d402b1b885
samsdaddd <guerquinsamuel@gmail.com>
2025-07-21 12:35:48 +0300

    Create base_medicaments.py

---
+++ base_medicaments.py
@@ -0,0 +1,11 @@
+ACB_DATABASE = {
+    "diphenhydramine": 3,
+    "amitriptyline": 3,
+    "oxybutynine": 3,
+    "paroxetine": 3,
+    "cyclobenzaprine": 2,
+    "loratadine": 1,
+    "cetirizine": 1,
+    "furosemide": 0,
+    "paracetamol": 0
+}
